8019835afeb42ef5e9e1322339c682fa5201635f
Milktrader <milktrader@gmail.com>
2011-10-21 08:20:13 -0400

    code for gaps on expiration friday

--- milktrader/README.markdown
+++ milktrader/README.markdown
@@ -1,6 +1,5 @@
-This is the correct code for 0ef3d72a57. 
+Expiration Friday has always had the feel that things are going to
+end up higher. It doesn't always work out that way, but that seems to be
+the bias. 
 
-The exponentiation of past returns was not in the code. It is now
-hard-coded in the **wasin** function. 
-
-The distribution plot is not affected by the error. 
+Compare gap up end of day results versus gap down end of day results.
--- milktrader/wasin.r
+++ milktrader/wasin.r
@@ -10,14 +10,15 @@ wasin <- function(sym="^GSPC"){
 	
 # extras
 	
-	x$RET.W    <- weeklyReturn(x)
-	x$RET.M    <- monthlyReturn(x)
+        x$RET.W    <- weeklyReturn(x)
+	#x$RET.M    <- monthlyReturn(x)
 	x$MON      <- weekdays(time(x)) == "Monday"
 	x$WED      <- weekdays(time(x)) == "Wednesday"
 	x$FRI      <- weekdays(time(x)) == "Friday"
 	r.50       <- SMA(Cl(x), n=50) 
 	r.200      <- SMA(Cl(x) , n=200)
 	x$bull     <- ifelse(r.50 > r.200, 1, 0)
+	x$gap      <- Op(x) - lag(Cl(x))
 	
 	x$ago.1    <- lag(x$ret, k=1)           
 	x$ago.2    <- lag(x$ret, k=2)           
@@ -33,22 +34,22 @@ wasin <- function(sym="^GSPC"){
 	x$ago.12   <- lag(x$ret, k=12)   
 	x$ago.13   <- lag(x$ret, k=13)   
 	x$ago.14   <- lag(x$ret, k=14)   
-	x$ago.15   <- lag(x$ret, k=14)   
-	x$ago.16   <- lag(x$ret, k=15)   
-	x$ago.17   <- lag(x$ret, k=16)   
-	x$ago.18   <- lag(x$ret, k=17)   
-	x$ago.19   <- lag(x$ret, k=18)   
-	x$ago.20   <- lag(x$ret, k=19)   
-	x$ago.21   <- lag(x$ret, k=20)   
-	x$ago.22   <- lag(x$ret, k=21)   
-	x$ago.23   <- lag(x$ret, k=22)   
-	x$ago.24   <- lag(x$ret, k=23)   
-	x$ago.25   <- lag(x$ret, k=24)   
-	x$ago.26   <- lag(x$ret, k=25)   
-	x$ago.27   <- lag(x$ret, k=26)   
-	x$ago.28   <- lag(x$ret, k=27)   
-	x$ago.29   <- lag(x$ret, k=28)   
-	x$ago.30   <- lag(x$ret, k=29)
+	x$ago.15   <- lag(x$ret, k=15)   
+	x$ago.16   <- lag(x$ret, k=16)   
+	x$ago.17   <- lag(x$ret, k=17)   
+	x$ago.18   <- lag(x$ret, k=18)   
+	x$ago.19   <- lag(x$ret, k=19)   
+	x$ago.20   <- lag(x$ret, k=20)   
+	x$ago.21   <- lag(x$ret, k=21)   
+	x$ago.22   <- lag(x$ret, k=22)   
+	x$ago.23   <- lag(x$ret, k=23)   
+	x$ago.24   <- lag(x$ret, k=24)   
+	x$ago.25   <- lag(x$ret, k=25)   
+	x$ago.26   <- lag(x$ret, k=26)   
+	x$ago.27   <- lag(x$ret, k=27)   
+	x$ago.28   <- lag(x$ret, k=28)   
+	x$ago.29   <- lag(x$ret, k=29)   
+	x$ago.30   <- lag(x$ret, k=30)
 	   
   x$AGO.1    <- lag(x$RET, k=1)           
 	x$AGO.2    <- lag(x$RET, k=2)           
@@ -114,32 +115,32 @@ wasin <- function(sym="^GSPC"){
 	
 	
 	
-	x$was.1     <-     exp(x$ago.1)-1
-        x$was.2     <-     exp(x$ago.1 + x$ago.2)-1
-        x$was.3     <-     exp(x$ago.1 + x$ago.2 + x$ago.3)-1
-        x$was.4     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4)-1                 
-        x$was.5     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5)-1                 
-        x$was.6     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6)-1                 
-        x$was.7     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7)-1                 
-        x$was.8     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8)-1                 
-        x$was.9     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9)-1                
-        x$was.10    <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9 + x$ago.10)-1                 
-        x$was.30    <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9 + x$ago.10 +                
-                               x$ago.11 + x$ago.12 + x$ago.13 + x$ago.14 + x$ago.15 + x$ago.16 + x$ago.17 + x$ago.8 + x$ago.19 + x$ago.20 +               
+	x$WAS.1     <-     exp(x$ago.1)-1 # or x$AGO.1
+        x$WAS.2     <-     exp(x$ago.1 + x$ago.2)-1
+        x$WAS.3     <-     exp(x$ago.1 + x$ago.2 + x$ago.3)-1
+        x$WAS.4     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4)-1                 
+        x$WAS.5     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5)-1                 
+        x$WAS.6     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6)-1                 
+        x$WAS.7     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7)-1                 
+        x$WAS.8     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8)-1                 
+        x$WAS.9     <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9)-1                
+        x$WAS.10    <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9 + x$ago.10)-1                 
+        x$WAS.30    <-     exp(x$ago.1 + x$ago.2 + x$ago.3 + x$ago.4 + x$ago.5 + x$ago.6 + x$ago.7 + x$ago.8 + x$ago.9 + x$ago.10 +                
+                               x$ago.11 + x$ago.12 + x$ago.13 + x$ago.14 + x$ago.15 + x$ago.16 + x$ago.17 + x$ago.18 + x$ago.19 + x$ago.20 +               
                                x$ago.21 + x$ago.22 + x$ago.23 + x$ago.24 + x$ago.25 + x$ago.26 + x$ago.27 + x$ago.28 + x$ago.29 + x$ago.30)-1                 
  
   
-	x$in.2     <-     exp(x$ret + next.1)-1
-        x$in.3     <-     exp(x$ret + next.1 + next.2)-1
-        x$in.4     <-     exp(x$ret + next.1 + next.2 + next.3)-1
-        x$in.5     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4)-1                 
-        x$in.6     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5)-1                 
-        x$in.7     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6)-1                 
-        x$in.8     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7)-1                 
-        x$in.9     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8)-1                 
-        x$in.10    <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8 + next.9)-1                
-        x$in.30    <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8 + next.9 + next.10 +                
-                                    next.11 + next.12 + next.13 + next.14 + next.15 + next.16 + next.17 + next.8 + next.19 + next.20 +               
+	x$IN.2     <-     exp(x$ret + next.1)-1
+        x$IN.3     <-     exp(x$ret + next.1 + next.2)-1
+        x$IN.4     <-     exp(x$ret + next.1 + next.2 + next.3)-1
+        x$IN.5     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4)-1                 
+        x$IN.6     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5)-1                 
+        x$IN.7     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6)-1                 
+        x$IN.8     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7)-1                 
+        x$IN.9     <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8)-1                 
+        x$IN.10    <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8 + next.9)-1                
+        x$IN.30    <-     exp(x$ret + next.1 + next.2 + next.3 + next.4 + next.5 + next.6 + next.7 + next.8 + next.9 + next.10 +                
+                                    next.11 + next.12 + next.13 + next.14 + next.15 + next.16 + next.17 + next.18 + next.19 + next.20 +               
                                     next.21 + next.22 + next.23 + next.24 + next.25 + next.26 + next.27 + next.28 + next.29  )-1                 
 
         assign("fat.data", x, envir=.GlobalEnv)
