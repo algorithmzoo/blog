fef49947a84b8a4faef59b3d1345d053c8f6c296
Milktrader <milktrader@gmail.com>
2011-11-23 11:08:53 -0500

    completed code for monkey thanksgiving

--- milktrader/thanks.sh
+++ milktrader/thanks.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/sh
 
 cat thanks |
 grep -v IND |
@@ -6,3 +6,7 @@ cut -c 1-10 |
 sed 's/-/ /g' | 
 awk '{if($3 >= 21 && $3 < 28) print $1, $2, $3, "gobble"}' |
 sed 's/ /-/g' > thanksgiving
+
+
+# not implemented how to not separate $3 and gobble with a space
+# done manually in vi
