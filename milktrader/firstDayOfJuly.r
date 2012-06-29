36a10f2f21851b98a3a1f12374f5402906c2b903
Milktrader <milktrader@gmail.com>
2012-06-29 14:12:27 -0400

    first day of July SPY

--- milktrader/firstDayOfJuly.r
+++ milktrader/firstDayOfJuly.r
@@ -4,5 +4,5 @@ require(myfun)
 getSymbols('SPY', from='1900-01-01')
 SPY$next.day = lag(dailyReturn(Cl(SPY)), k=-1)
 spy          = apply.quarterly(SPY, last)
-
-PNGzooplot(spy$next.day, lastprice=135, title='July 3, 2012')
+july         = spy[.indexmon(spy) == 5]
+PNGzooplot(july$next.day, lastprice=135.5, title='July 3, 2012')
