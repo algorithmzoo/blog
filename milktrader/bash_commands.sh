f0420a925109513b86237f0b0877b70d7a318b93
Milktrader <milktrader@gmail.com>
2012-08-04 20:50:24 -0400

    shell processing of nfp dates

---
+++ milktrader/bash_commands.sh
@@ -0,0 +1,1 @@
+cat foo.csv | sed 's/"//g' | head -n 48 | tail -n 47 | sed 's/,NA//g' | sed 's/*//g' | cut -d ',' -f2-14 | cut -c 3- | awk -F ',' '{for(i = 2; i<=NF; i++) print $i "/" $1}' | cut -d '/' -f1-3 | head -n 559 > fixedfoo
