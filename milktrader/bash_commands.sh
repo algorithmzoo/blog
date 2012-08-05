6944c727603a48f7d4ea8a8cae91c4eb2116d1ca
Milktrader <milktrader@gmail.com>
2012-08-04 21:46:17 -0400

    SPX returns day after NFP in August

--- milktrader/bash_commands.sh
+++ milktrader/bash_commands.sh
@@ -1,1 +1,11 @@
-cat foo.csv | sed 's/"//g' | head -n 48 | tail -n 47 | sed 's/,NA//g' | sed 's/*//g' | cut -d ',' -f2-14 | cut -c 3- | awk -F ',' '{for(i = 2; i<=NF; i++) print $i "/" $1}' | cut -d '/' -f1-3 | head -n 559 > fixedfoo
+cat foo.csv | 
+sed 's/"//g' | 
+head -n 48 | 
+tail -n 47 | 
+sed 's/,NA//g' | 
+sed 's/*//g' | 
+cut -d ',' -f2-14 | 
+cut -c 3- | 
+awk -F ',' '{for(i = 2; i<=NF; i++) print $i "/" $1}' | 
+cut -d '/' -f1-3 | 
+head -n 559 > fixedfoo
