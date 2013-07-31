ff7804dfe214563533fca62c2a2cb221c6d23e97
milktrader <milktrader@gmail.com>
2013-07-31 10:11:58 -0400

    cleanup from 8 months ago

--- milktrader/ndxMonthly.r
+++
@@ -1,7 +0,0 @@
-getSymbols('^NDX', from='1900-01-01')
-N = monthlyReturn(Cl(NDX))
-ndx = N[.indexmon(N) == 11]
-png('decNDX.png')
-zeroplot(ndx, xlab='from 1985 to present', up.col='yellow', dn.col='yellow4', main="NDX")
-dev.off()
-
--- milktrader/spxMonthly.r
+++
@@ -1,6 +0,0 @@
-getSymbols('^GSPC', from='1900-01-01')
-G = monthlyReturn(Cl(GSPC))
-g = G[.indexmon(G) == 11]
-png('decSPX.png')
-zeroplot(g, xlab='from 1950 to present')
-dev.off()
